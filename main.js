0d8f70adccf364ffa1fc9846f1d13c0f9ba963c8
Leonardino80 <gilgamesh2005@gmail.com>
2019-05-13 05:07:15 +0200

    consegna

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 $('.next').click(function(){
 
   var img_current = $('.visibile');
+  var space_current = $('.indicatore');
 
-  var next_img = img_current.next();
+  var next_img = img_current.next('img');
+  var next_space = space_current.next('div');
 
   img_current.toggleClass('visibile');
+  space_current.toggleClass('indicatore');
 
-  next_img.toggleClass('visibile');
+  if(next_img.length != 0) {
+    next_img.toggleClass('visibile');
+    next_space.toggleClass('indicatore');
+
+  } else {
+    $('.slider img').first().toggleClass('visibile');
+    $('.pianta div').first().toggleClass('indicatore');
+  }
 
 });
 
 $('.prev').click(function(){
 
   var img_current = $('.visibile');
+  var space_current = $('.indicatore');
 
-  var prev_img = img_current.prev();
+  var prev_img = img_current.prev('img');
+  var prev_space = space_current.prev('div');
 
   img_current.toggleClass('visibile');
+  space_current.toggleClass('indicatore');
 
-  prev_img.toggleClass('visibile');
+  if(prev_img.length != 0) {
+    prev_img.toggleClass('visibile');
+    prev_space.toggleClass('indicatore');
+
+  } else {
+    $('.slider img').last().toggleClass('visibile');
+    $('.pianta div').last().toggleClass('indicatore');
+  }
 
 });
 
